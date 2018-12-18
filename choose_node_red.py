8da29afcebd6ce1167438902cfbaf5391eba20cf
Clement Beal <clement.beal@etu.unice.fr>
2018-12-18 17:27:35 +0100

    Ajout du node-red IHM

--- choose_node_red.py
+++ choose_node_red.py
@@ -3,11 +3,12 @@ import os
 
 
 def executeNodeRed(name):
-	print(name)
-	if(name == "server"):
+	if name == "server":
 		os.system('node-red --userDir NR_orchestreur/ flow.json')
-	else:
+	if name == "rasp":
 		os.system('node-red --userDir NR_raspberry/ flow.json')
+	if name == "ihm":
+		os.system('node-red --userDir NR_IHM/ flow.json')
 
 if __name__ == "__main__":
     if len(sys.argv) == 2 :
